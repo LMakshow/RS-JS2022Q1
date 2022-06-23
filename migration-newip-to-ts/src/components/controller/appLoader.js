3172bb30e7a1e931db500d66d98e464e7f2920f5
Maksym Lytvyn <makshow.at@gmail.com>
2022-06-24 00:13:08 +0300

    refactor: apply eslint and webpack for typescript

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '0b493ad2690e4afcbe87fad6033fe24b', // получите свой ключ https://newsapi.org/
         });
     }
 }
